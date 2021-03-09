f1ed366d9e13ed3b9004b696bec9d93a850226af
Vicktor Hutama <vicktor.hutama@gmail.com>
2021-03-09 11:36:23 +0700

    Updated home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,16 +5,25 @@
         <img alt="Quasar logo" src="~assets/logo-firebase.png" />
       </div>
       <div class="q-px-lg text-h2 text-center">Quasar Firebase Template</div>
+      <div class="q-px-lg q-mt-md text-h6 text-center">
+        <a
+          target="_blank"
+          href="https://github.com/vicktorhu/quasar-firebase-template"
+          class="text-primary"
+          style="text-decoration: none;"
+          >Github Repository</a
+        >
+      </div>
       <div class="q-px-lg q-mt-md text-h7 text-center">
         By Vicktor Hutama (<a
           target="_blank"
-          href="https://github.com/vicktorhu/quasar-firebase-template"
+          href="https://github.com/vicktorhu/"
           class="text-primary"
           >vicktorhu</a
         >) | Written for
         <a
           target="_blank"
-          href="https://github.com/vicktorhu/quasar-firebase-template"
+          href="https://next.quasar.dev"
           class="text-primary"
           >Quasar v2</a
         >
